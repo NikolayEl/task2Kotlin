f116bed320e2ebe669cfe739a280afe70d6cbc96
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-10 19:25:00 +0800

    task8-2 is done

--- 8-collections_and_loops/src/Task2.kt
+++ 8-collections_and_loops/src/Task2.kt
@@ -1,3 +1,5 @@
+import kotlin.math.E
+
 /*
 Дан класс, описывающий сотрудника:
 data class Employee(val name: String, val age: Int)
@@ -10,4 +12,21 @@ data class Employee(val name: String, val age: Int)
 
 fun main() {
 
+    val user1 = Employee("Nikolay", 40)
+    val user2 = Employee("Olga", 10)
+    val user3 = Employee("Vlad", 11)
+    val user4 = Employee("Nikita", 12)
+
+    val listUser: List<Employee> = listOf(user1, user2, user3, user4)
+
+    println("Emloyee with the lowest age: ${getMinAgeUser(listUser).name}")
+}
+
+fun getMinAgeUser(listUser: List<Employee>): Employee {
+    var minAgeUser: Employee = listUser[0]
+    for (user in listUser) {
+        if (minAgeUser.age > user.age)
+            minAgeUser = user
+    }
+    return minAgeUser
 }
