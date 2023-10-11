729eba5b8c4d0ed14b6fbcd000594bc15d3b8e81
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-11 14:22:08 +0800

    task9-3 is done

--- 9-lambdas/src/Task3.kt
+++ 9-lambdas/src/Task3.kt
@@ -11,4 +11,19 @@ fun elementsCount(users: List<User>, condition: (User) -> Boolean): Int
 
 fun main() {
 
+    val user1 = User("Nikolay", 40)
+    val user2 = User("Olga", 37)
+    val user3 = User("Vladislav", 11)
+
+    val users: List<User> = listOf(user1, user2, user3)
+
+    println("Users over 18: ${elementsCountCondition(users) { it -> it.age > 18 }}")
+}
+
+fun elementsCountCondition(users: List<User>, condition: (User) -> Boolean): Int {
+    var count: Int = 0
+    for (user in users)
+        if (condition(user))
+            count++
+    return count
 }
