75977bf760b4ec2902151ebc7903a3fa7184c858
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-11 12:19:02 +0800

    task9-2 is done

--- 9-lambdas/src/Task2.kt
+++ 9-lambdas/src/Task2.kt
@@ -11,5 +11,24 @@ fun containsElement(users: List<User>, condition: (User) -> Boolean): Boolean
 */
 
 fun main() {
+    val user1 = User("Nikolay", 40)
+    val user2 = User("Olga", 37)
+    val user3 = User("Vladislav", 11)
 
+    val users = listOf(user1, user2, user3)
+
+    val symbol: Char = 'P'
+    if (containsElement(users) { it.name.contains(symbol) })
+        println("The letter N is in this list users.")
+    else
+        println("The letter N is not in this list users.")
+
+
+}
+
+fun containsElement(users: List<User>, condition: (User) -> Boolean): Boolean {
+    for (user in users)
+        if (condition(user))
+            return true
+    return false
 }
