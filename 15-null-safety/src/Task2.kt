76056044d482d9d17d72f6988c01cd761ca1d1cb
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-14 17:35:49 +0800

    task15-2 is done

--- 15-null-safety/src/Task2.kt
+++ 15-null-safety/src/Task2.kt
@@ -11,9 +11,27 @@ class Employee(val name: String, val company: Company?)
 
 Для этой задачи есть видео с разбором.
 */
-
+fun employeeCity(employee: Employee): String{
+    return "Человек по имени ${employee.name} работает в компании: ${employee.company?.name?: "Не работает"}," +
+            " располагающаяся в городе: ${employee.company?.address?.city?: "Неизвестен"}, " +
+            "по адресу: ${employee.company?.address?.address?:"Неизвестен"}"
+}
 fun main() {
 
+    val adress1 = Address("December Events street, house 123, flat 12", "Irkutsk")
+    val adress2 = Address("Yamskaya street, house 33, flat 41", "Irkutsk")
+    val adress3 = Address("Subpharmacy street, house 1, flat 42", "Irkutsk")
+
+    val companyShopOnline = Company("Wildberies", null)
+    val companyPolice = Company("Police", adress3)
+    val companyShop = Company("Amber", adress1)
+
+    val employee1 = Employee("Nikolay", companyPolice)
+    val employee2 = Employee("Olga", companyShopOnline)
+    val employee3 = Employee("Akeksey", companyShop)
+    val employee4 = Employee("Vladislav", null)
+
+    println(employeeCity(employee4))
 }
 
 class Address(val address: String, val city: String)
